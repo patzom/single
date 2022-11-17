0a851e5193c4606d9e4a1587da2f5bc98ea22097
unknown <labor@Derma148-060.derma.meduniwien.ac.at>
2022-11-17 14:44:35 +0100

    subset testing

--- single/R/0_data.R
+++ single/R/0_data.R
@@ -1,12 +1,13 @@
 ###data
-
+library(Seurat)
 #load data
 
 tabib.data  <- Read10X("C:/Users/labor/Desktop/patrick/single/single/data/set_4/MD001_DK1/filtered_feature_bc_matrix")
 
 
 #set up tabib data
-tabib.keloid <- CreateSeuratObject(counts=test , assay="RNA")
+tabib.keloid <- CreateSeuratObject(counts=tabib.data , assay="RNA")
 
 #subset samples
 
+tabib.keloid[c("CD3D", "TCL1A", "MS4A1"), 1:300]
